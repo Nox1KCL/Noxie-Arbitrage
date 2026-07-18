c17e5959bc7ea04094523efb6695edf93596501c
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-18 21:55:45 +0300

    feat!&refactor: realize logger based on Lumberjack and move logs directory to project root

---
+++ internal/logger/logger.go
@@ -0,0 +1,99 @@
+// Package logger provides logging utilities for the InFolderSort application.
+package logger
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"path/filepath"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var llog = slog.With("module", "logger")
+
+type LumberConfig struct {
+	MaxSize    int  `toml:"max_size"`
+	MaxAge     int  `toml:"max_age"`
+	MaxBackups int  `toml:"max_backups"`
+	Compress   bool `toml:"compress"`
+}
+
+type LeveledHandler struct {
+	handlers []handlerEntry
+}
+
+type handlerEntry struct {
+	level   slog.Level
+	handler slog.Handler
+}
+
+func (h *LeveledHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	for _, entry := range h.handlers {
+		if entry.handler.Enabled(ctx, level) {
+			return true
+		}
+	}
+	return false
+}
+
+func (h *LeveledHandler) Handle(ctx context.Context, r slog.Record) error {
+	var errs []error
+	for _, entry := range h.handlers {
+		if entry.handler.Enabled(ctx, r.Level) {
+			if err := entry.handler.Handle(ctx, r); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+	err := errors.Join(errs...)
+	return err
+}
+
+func (h *LeveledHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	var newHandles []handlerEntry
+	for _, entry := range h.handlers {
+		newHandles = append(newHandles, handlerEntry{entry.level, entry.handler.WithAttrs(attrs)})
+	}
+	return &LeveledHandler{handlers: newHandles}
+}
+func (h *LeveledHandler) WithGroup(name string) slog.Handler {
+	var newHandles []handlerEntry
+	for _, entry := range h.handlers {
+		newHandles = append(newHandles, handlerEntry{entry.level, entry.handler.WithGroup(name)})
+	}
+	return &LeveledHandler{handlers: newHandles}
+}
+
+func GetHandler(cfg *LumberConfig, levels map[slog.Level]string) (*LeveledHandler, error) {
+	var handlers []handlerEntry
+	for level, path := range levels {
+		handler, err := HandlerConveyor(path, level, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("creating logger %q: %w", path, err)
+		}
+		handlers = append(handlers, handlerEntry{level, handler})
+	}
+
+	logHandler := &LeveledHandler{handlers}
+
+	llog.Info("logger handlers created successfully", "levels_count", len(levels))
+	return logHandler, nil
+}
+
+func HandlerConveyor(filename string, level slog.Level, cfg *LumberConfig) (slog.Handler, error) {
+	absInfoPath, err := filepath.Abs(filename)
+	if err != nil {
+		return nil, fmt.Errorf("getting abs path %q: %w", absInfoPath, err)
+	}
+	logger := &lumberjack.Logger{
+		Filename:   absInfoPath,
+		MaxSize:    cfg.MaxSize,
+		MaxAge:     cfg.MaxAge,
+		MaxBackups: cfg.MaxBackups,
+		Compress:   cfg.Compress,
+	}
+	handler := slog.NewJSONHandler(logger, &slog.HandlerOptions{Level: level})
+	return handler, nil
+}
