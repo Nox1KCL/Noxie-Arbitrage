2bb8a9c0763a618db24a46572fa54487da175c2c
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-20 13:09:27 +0300

    feat: realize src structure template and implemented broker

---
+++ parsers/src/parsers/broker.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+import aio_pika
+from aio_pika.abc import AbstractChannel, AbstractQueue
+
+@dataclass
+class Broker():
+    connection: aio_pika.Connection | None = None
+    channel: AbstractChannel | None = None
+    queue_name: str = ""
+
+    async def load(self, body: bytes):
+        assert self.channel is not None, "connect() first"
+
+        _ = await self.channel.default_exchange.publish(
+            aio_pika.Message(body),
+            routing_key=self.queue_name,
+        )
+
+    async def unload(self) -> AbstractQueue | None:
+        assert self.channel is not None, "connect() first"
+        try:
+            return await self.channel.get_queue(
+                self.queue_name,
+                ensure=True
+            )
+        except aio_pika.exceptions.ChannelNotFoundEntity:
+            return None
+
+    async def make_queue(self, queue_name: str):
+        assert self.channel is not None, "connect() first"
+
+        self.queue_name = queue_name
+        _ = await self.channel.declare_queue(
+            queue_name,
+            durable=True
+        )
+
+    async def connect(self, url: str):
+        self.connection = await aio_pika.connect_robust(url)
+        self.channel = await self.connection.channel()
+
+    async def tidy(self):
+        if self.channel:
+            await self.channel.close()
+        if self.connection:
+            await self.connection.close()
---
+++ parsers/src/parsers/utils.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent.parent
+
+async def get_env():
+    _ = load_dotenv(PROJECT_ROOT / ".env")
+    return
+
+async def get_url() -> str:
+    host = os.getenv("RABBIT_HOST")
+    pwd = os.getenv("RABBIT_PWD")
+    user = os.getenv("RABBIT_USER")
+    port = os.getenv("RABBIT_PORT")
+
+    url = f"amqp://{user}:{pwd}@{host}:{port}/"
+    return url
