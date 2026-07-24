9e8fe92fc481bf008e16893963303a877b302a05
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-24 22:27:01 +0300

    feat: write basic Ticker model with pydantic

---
+++ parsers/src/parsers/scrapers/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+class TickerForm(BaseModel):
+    exchange_name: str = Field(alias="exchangeName")
+    symbol: str
+    current_price: float = Field(ge=0, alias="currentPrice")
+    best_ask: float = Field(ge=0, alias="bestAsk")
+    best_bid: float = Field(ge=0, alias="bestBid")
+    volume: float = Field(ge=0)
+    timestamp: int = Field(ge=0)
