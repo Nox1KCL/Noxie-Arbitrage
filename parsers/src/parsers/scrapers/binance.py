321c50a14229c0f604073dad65da89ba32312c2e
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-25 15:27:50 +0300

    refactor: move all client.get logic to get_data func in utils for more readable

--- parsers/src/parsers/scrapers/binance.py
+++ parsers/src/parsers/scrapers/binance.py
@@ -1,6 +1,8 @@
 from typing import override
 import time
 
+from parsers.utils import get_data
+from parsers.config.config import HttpConfig
 from parsers.scrapers.models import TickerForm
 from parsers.scrapers.base import BasicScraper
 from parsers.broker import Broker
@@ -11,14 +13,11 @@ class BinanceScraper(BasicScraper):
         super().__init__(client, broker, api_url)
 
     @override
-    async def fetch_data(self, symbol: str) -> TickerForm:
-        response = await self.client.get(self.api_url)
-
-        if response.status_code != 200:
-            # TODO: створити власний ексепшн
-            raise Exception(f"Failed to fetch data: {response.status_code}")
-
+    async def fetch_data(self, symbol: str, cfg: HttpConfig) -> TickerForm:
+        target_url = f"{self.api_url}{symbol}"
+        response = await get_data(target_url, self.client, cfg)
         raw_data = response.json()
+
         try:
             return TickerForm(
                 exchangeName="Binance",
