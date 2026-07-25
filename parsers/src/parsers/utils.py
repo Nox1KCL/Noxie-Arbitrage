3d99d61525e448848e59cdddab813f2637eb0828
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-25 12:15:09 +0300

    fix: remove unnecessary async from funcs

--- parsers/src/parsers/utils.py
+++ parsers/src/parsers/utils.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent.parent
 
-async def get_env():
+def get_env():
     _ = load_dotenv(PROJECT_ROOT / ".env")
     return
 
-async def get_url() -> str:
+def get_url() -> str:
     host = os.getenv("RABBIT_HOST")
     pwd = os.getenv("RABBIT_PWD")
     user = os.getenv("RABBIT_USER")
