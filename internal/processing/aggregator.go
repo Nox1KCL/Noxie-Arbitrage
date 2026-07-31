37ffd23f1dfc9f42cd9ca9905c9d9b3a4634607c
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-31 12:28:14 +0300

    feat!: implement aggregator for processing spread, also use it in filter.go, the result sends to payload (sender.go)

--- internal/processing/aggregator.go
+++ internal/processing/aggregator.go
@@ -1,5 +1,95 @@
 package processing
 
+import (
+	"time"
+
+	"github.com/Nox1KCL/Arbitrage/internal/database/models"
+)
+
 type Aggregator struct {
-    
+	Ticks map[string]map[string]*TickerForm // Symbol / Exchanges
+	Maps  *models.CachedMaps
+}
+
+type Spread struct {
+	BuyExchange  string
+	SellExchange string
+	BuyPrice     float64
+	SellPrice    float64
+	Spread       float64
+}
+
+func NewAggregator(maps *models.CachedMaps) *Aggregator {
+	return &Aggregator{
+		Ticks: make(map[string]map[string]*TickerForm),
+		Maps:  maps,
+	}
+}
+
+func Aggregation(agg *Aggregator, t *TickerForm) *Spread {
+	_, exists := agg.Maps.InterestedSymbols[t.Symbol]
+	if !exists {
+		return nil
+	}
+	if agg.Ticks[t.Symbol] == nil {
+		agg.Ticks[t.Symbol] = make(map[string]*TickerForm)
+	}
+	agg.Ticks[t.Symbol][t.ExchangeName] = t
+	if len(agg.Ticks[t.Symbol]) < 2 {
+		return nil
+	}
+
+	agg.TTLChecker()
+	spread := agg.Arbitrage(t.Symbol)
+	if spread != nil {
+		return spread
+	}
+	return nil
+}
+
+func (agg *Aggregator) TTLChecker() {
+	for _, batch := range agg.Ticks {
+		for exchange, t := range batch {
+			if time.Since(time.UnixMilli(t.Timestamp)) >= 5*time.Second {
+				delete(batch, exchange)
+			}
+		}
+	}
+
+}
+
+func (agg *Aggregator) Arbitrage(symbol string) *Spread {
+	var (
+		minAskTicker TickerForm
+		maxBidTicker TickerForm
+		first        = true
+	)
+	exchanges := agg.Ticks[symbol]
+	for _, t := range exchanges {
+		if first {
+			minAskTicker = *t
+			maxBidTicker = *t
+			first = false
+			continue
+		}
+		if t.BestAsk < minAskTicker.BestAsk {
+			minAskTicker = *t
+		}
+		if t.BestBid > maxBidTicker.BestBid {
+			maxBidTicker = *t
+		}
+	}
+	if maxBidTicker.BestBid > minAskTicker.BestAsk && minAskTicker.ExchangeName != maxBidTicker.ExchangeName {
+		buyPrice := minAskTicker.BestAsk
+		sellPrice := maxBidTicker.BestBid
+		spread := ((sellPrice - buyPrice) / buyPrice) * 100
+		return &Spread{
+			BuyExchange:  minAskTicker.ExchangeName,
+			SellExchange: maxBidTicker.ExchangeName,
+			BuyPrice:     buyPrice,
+			SellPrice:    sellPrice,
+			Spread:       spread,
+		}
+	}
+	return nil
 }
--- internal/processing/filter.go
+++ internal/processing/filter.go
@@ -3,22 +3,30 @@ package processing
 import (
 	"context"
 	"encoding/json"
+
+	"github.com/Nox1KCL/Arbitrage/internal/database/models"
 )
 
-func Filter(ctx context.Context, channel <-chan TickerForm, payload chan<- []byte) {
-    for {
-        select {
-            case <-ctx.Done():
-                return
-            case data, ok := <-channel:
-                if !ok {
-                    return
-                }
-                bytesData, err := json.Marshal(data)
-                if err != nil {
-                    return
-                }
-                payload <- bytesData
-        }
-    }
+func Filter(ctx context.Context, channel <-chan TickerForm, payload chan<- []byte, maps *models.CachedMaps) {
+	aggregator := NewAggregator(maps)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case data, ok := <-channel:
+			if !ok {
+				return
+			}
+
+			spread := Aggregation(aggregator, &data)
+			if spread == nil {
+				continue
+			}
+			bytesData, err := json.Marshal(&spread)
+			if err != nil {
+				return
+			}
+			payload <- bytesData
+		}
+	}
 }
