6999ed2a5c9507dfa52da6fbd14dc2a0c2dbeb12
Nox1KCL <korshunov.maks09@gmail.com>
2026-07-31 12:23:40 +0300

    chores: use go fmt for pretty code

--- internal/processing/sender.go
+++ internal/processing/sender.go
@@ -11,24 +11,24 @@ import (
 var selog = slog.With("service", "processing")
 
 func Sending(ctx context.Context, client pb.DataServiceClient, payload chan []byte) {
-    for {
-        select {
-            case <-ctx.Done():
-                return
-            case p := <-payload:
-               	req := &pb.User{
-                   	UserData: p,
-                }
-                _ = gRPCsender(ctx, client, req)
-        }
-    }
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case p := <-payload:
+			req := &pb.User{
+				UserData: p,
+			}
+			_ = gRPCsender(ctx, client, req)
+		}
+	}
 }
 
 func gRPCsender(ctx context.Context, client pb.DataServiceClient, req *pb.User) *pb.Ack {
-    callCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
-    defer cancel()
+	callCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 
-    res, err := client.SendUser(callCtx, req)
+	res, err := client.SendUser(callCtx, req)
 	if err != nil {
 		selog.Error("Could not send user", "error", err)
 		return nil
