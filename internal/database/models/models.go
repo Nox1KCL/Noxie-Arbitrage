a32fefe6581a7ecabc5101c103595c61ab536694
Nox1KCL <korshunov.maks09@gmail.com>
2026-08-05 10:54:41 +0300

    refactor(models): remove unnecessary Exchanges field in Subscription struct

--- internal/database/models/models.go
+++ internal/database/models/models.go
@@ -1,13 +1,12 @@
 package models
 
 type Subscription struct {
-	ID                    uint     `gorm:"primaryKey"`
-	TelegramChatID        int64    `gorm:"not null;index;uniqueIndex:idx_user_sym"`
-	Symbol                string   `gorm:"not null;index;uniqueIndex:idx_user_sym"`
-	MinSpreadPercent      float64  `gorm:"not null"`
-	MinVolume             float64  `gorm:"not null"`
-	MinPriceChangePercent float64  `gorm:"not null"`
-	// Exchanges             []string `gorm:"text[]"`
+	ID                    uint    `gorm:"primaryKey"`
+	TelegramChatID        int64   `gorm:"not null;index;uniqueIndex:idx_user_sym"`
+	Symbol                string  `gorm:"not null;index;uniqueIndex:idx_user_sym"`
+	MinSpreadPercent      float64 `gorm:"not null"`
+	MinVolume             float64 `gorm:"not null"`
+	MinPriceChangePercent float64 `gorm:"not null"`
 }
 
 type CachedMaps struct {
