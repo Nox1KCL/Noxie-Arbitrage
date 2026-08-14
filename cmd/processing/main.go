4ee75d792d079c6dbdb6d439660336a7f83fd9b2
Nox1KCL <korshunov.maks09@gmail.com>
2026-08-14 15:19:36 +0300

    refactor: add context to logs, change sending func signature, add gracefully shutdown

--- cmd/processing/main.go
+++ cmd/processing/main.go
@@ -15,8 +15,10 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/Nox1KCL/Arbitrage/internal/broker"
+	"github.com/Nox1KCL/Arbitrage/internal/config"
 	"github.com/Nox1KCL/Arbitrage/internal/database"
 	"github.com/Nox1KCL/Arbitrage/internal/database/models"
+	telemetry "github.com/Nox1KCL/Arbitrage/internal/observer"
 	"github.com/Nox1KCL/Arbitrage/internal/processing"
 	"github.com/Nox1KCL/Arbitrage/internal/syncutils"
 	"github.com/Nox1KCL/Arbitrage/internal/transport"
@@ -42,26 +44,31 @@ func (s *server) ReloadSubscriptions(ctx context.Context, _ *emptypb.Empty) (*pb
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
 	db, err := database.Connect()
 	if err != nil {
-		plog.Error("Could not connect to db", "error", err)
+		plog.ErrorContext(ctx, "Could not connect to db", "error", err)
 		os.Exit(1)
 	}
 	if err := db.AutoMigrate(&models.Subscription{}); err != nil {
-		plog.Error("Could not create a table", "error", err)
+		plog.ErrorContext(ctx, "Could not create a table", "error", err)
 		os.Exit(1)
 	}
 	store := &processing.SubscriptionStore{}
 	err = store.Reload(db)
 	if err != nil {
-		plog.Error("Could not reload store", "error", err)
+		plog.ErrorContext(ctx, "Could not reload store", "error", err)
 		os.Exit(1)
 	}
 
 	listener, err := net.Listen("tcp", ":50050")
 	if err != nil {
-		plog.Error("Could not listen on port 50050", "error", err)
-		return
+		plog.ErrorContext(ctx, "Could not listen on port 50050", "error", err)
+		os.Exit(1)
 	}
 	grpcServer := grpc.NewServer()
 	pb.RegisterProcessingServiceServer(grpcServer, &server{db: db, store: store})
@@ -72,52 +79,69 @@ func main() {
 	var serviceWg syncutils.MyWaitGroup
 	serviceWg.Go(func() {
 		if err := grpcServer.Serve(listener); err != nil {
-			plog.Error("grpc server error", "error", err)
+			plog.ErrorContext(ctx, "grpc server error", "error", err)
+			os.Exit(1)
 		}
 	})
 
 	conn, err := grpc.NewClient("delivery-go:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		plog.Error("Could not connect to client", "error", err)
-		return
+		plog.ErrorContext(ctx, "Could not connect to client", "error", err)
+		os.Exit(1)
 	}
 	defer conn.Close()
 
 	client := pb.NewDataServiceClient(conn)
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
 	b, err := broker.NewBroker("parser.binance.ticker")
 	if err != nil {
-		plog.Error("Could not create broker", "error", err)
+		plog.ErrorContext(ctx, "Could not create broker", "error", err)
 		os.Exit(1)
 	}
+
+	cfg, err := config.GetConfig("config.toml")
+	if err != nil {
+		plog.ErrorContext(ctx, "getting config", "error", err)
+		os.Exit(1)
+	}
+
+	shutdown, obs, err := telemetry.NewTelemetry(&cfg.LumberConfig)
+	if err != nil {
+		plog.ErrorContext(ctx, "getting telemetry", "error", err)
+		os.Exit(1)
+	}
+	defer shutdown(ctx)
 
 	msgs, err := b.Unload()
 	if err != nil {
-		plog.Error("Could not unload broker", "error", err)
+		plog.ErrorContext(ctx, "Could not unload broker", "error", err)
 		os.Exit(1)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-
 	tickerChannel := make(chan processing.TickerForm)
 	serviceWg.Go(func() {
-		processing.Scanner(ctx, msgs, tickerChannel)
+		plog.InfoContext(ctx, "Starting scanner process")
+		processing.Scanner(ctx, msgs, tickerChannel, obs)
 	})
 
 	payload := make(chan []*transport.FormedMessage)
 	serviceWg.Go(func() {
-		processing.Filter(ctx, tickerChannel, payload, store)
+		plog.InfoContext(ctx, "Starting filter process")
+		processing.Filter(ctx, tickerChannel, payload, store, obs)
 	})
 
+	s := &processing.SendingServer{
+		Client: client,
+		Obs:    obs,
+	}
+
 	serviceWg.Go(func() {
-		processing.Sending(ctx, client, payload)
+		plog.InfoContext(ctx, "Starting sending process")
+		processing.Sending(ctx, payload, s)
 	})
 
 	sig := <-sigChan
-	plog.Warn("Received signal", "signal", sig)
+	plog.WarnContext(ctx, "Received signal", "signal", sig)
 
 	cancel()
 	serviceWg.Wait()
