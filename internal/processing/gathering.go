872497875a9040cb985213e023a5c088ffa812bd
Nox1KCL <korshunov.maks09@gmail.com>
2026-08-14 21:05:23 +0300

    feat(processing): add metrics

--- internal/processing/gathering.go
+++ internal/processing/gathering.go
@@ -6,7 +6,9 @@ import (
 
 	telemetry "github.com/Nox1KCL/Arbitrage/internal/observer"
 	"github.com/rabbitmq/amqp091-go"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type TickerForm struct {
@@ -20,6 +22,12 @@ type TickerForm struct {
 }
 
 func Scanner(ctx context.Context, msgs <-chan amqp091.Delivery, channel chan<- TickerForm, obs *telemetry.Observe) {
+	metrics, err := NewProcessingMetrics(obs.Meter)
+	if err != nil {
+		plog.ErrorContext(ctx, "could not init processing metrics", "error", err)
+		return
+	}
+
 	for {
 		select {
 		case <-ctx.Done():
@@ -28,27 +36,37 @@ func Scanner(ctx context.Context, msgs <-chan amqp091.Delivery, channel chan<- T
 			if !ok {
 				return
 			}
-			ticker := gatherProcessing(ctx, data, obs)
+			ticker := gatherProcessing(ctx, data, obs, metrics)
 			if ticker == (TickerForm{}) {
 				continue
 			}
+
+			metrics.counter.Add(ctx, 1, metric.WithAttributes(
+				attribute.String("stage", "scanning"),
+				attribute.String("type", "sent.data"),
+			))
 			channel <- ticker
 		}
 	}
 }
 
-func gatherProcessing(ctx context.Context, data amqp091.Delivery, obs *telemetry.Observe) TickerForm {
-	_, span := obs.Tracer.Start(ctx, "GatherMessage")
+func gatherProcessing(ctx context.Context, data amqp091.Delivery, obs *telemetry.Observe, m *processingMetrics) TickerForm {
+	ctx, span := obs.Tracer.Start(ctx, "GatherMessage")
 	defer span.End()
 
 	var ticker TickerForm
 	if err := json.Unmarshal(data.Body, &ticker); err != nil {
 		span.SetStatus(codes.Error, "failed to ummarshal message")
 		span.RecordError(err)
+		m.counter.Add(ctx, 1, metric.WithAttributes(
+			attribute.String("stage", "gathering"),
+			attribute.String("type", "failed.message.packaging"),
+		))
 
 		_ = data.Nack(false, false)
 		return TickerForm{}
 	}
+
 	_ = data.Ack(false)
 	return ticker
 }
