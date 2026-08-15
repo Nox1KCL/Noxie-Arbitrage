e9864d7151de7b7832bfdfad47a0adf5da0a7eaa
Nox1KCL <korshunov.maks09@gmail.com>
2026-08-15 14:56:03 +0300

    feat(delivery): add telemetry to delivery (SendUser, sendTelegramMessage), also tracing from main

--- cmd/delivery/main.go
+++ cmd/delivery/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net"
 	"os"
@@ -13,29 +14,72 @@ import (
 	"github.com/Nox1KCL/Arbitrage/internal/syncutils"
 	pb "github.com/Nox1KCL/Arbitrage/internal/transport/proto"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 var dlog = slog.With("service", "delivery")
 
+type deliveryMetrics struct {
+	counter   metric.Int64Counter
+	histogram metric.Float64Histogram
+}
+
+func NewDeliveryMetrics(meter metric.Meter) (*deliveryMetrics, error) {
+	counter, err := meter.Int64Counter(
+		"counter",
+		metric.WithDescription("for counting sent messages, errors"),
+	)
+	if err != nil {
+		err := fmt.Errorf("counter init failed: %w", err)
+		return nil, err
+	}
+
+	histogram, err := meter.Float64Histogram(
+	"histogram",
+		metric.WithDescription("for calculating averange stats"),
+	)
+	if err != nil {
+		err := fmt.Errorf("histogram init failed: %w", err)
+		return nil, err
+	}
+
+	metrics := &deliveryMetrics{
+		counter: counter,
+		histogram: histogram,
+	}
+	return metrics, nil
+}
+
 type server struct {
 	pb.UnimplementedDataServiceServer
+	obs *telemetry.Observe
+	metrics *deliveryMetrics
 }
 
 // TODO: Придумати як застовувати сервіс тут, також закомітити зміни
 func (s *server) SendUser(ctx context.Context, req *pb.AlertNotification) (*pb.Ack, error) {
-	if err := sendTelegramMessage(ctx, req.GetTelegramChatId(), req.GetText()); err != nil {
+	if err := sendTelegramMessage(ctx, req.GetTelegramChatId(), req.GetText(), s.obs, s.metrics); err != nil {
 		dlog.Error("Could not send telegram message",
 			"error", err,
 			"chat_id", req.GetTelegramChatId(),
 		)
+		s.metrics.counter.Add(ctx, 1, metric.WithAttributes(
+		attribute.String("stage", "sendTelegramMessage"),
+			attribute.String("type", "grpc.sent.error"),
+		))
 
 		return &pb.Ack{
 			Status:  false,
 			Details: err.Error(),
 		}, nil
 	}
+
+	s.metrics.counter.Add(ctx, 1, metric.WithAttributes(
+		attribute.String("stage", "sendTelegramMessage"),
+		attribute.String("type", "grpc.sent.success"),
+	))
 
 	return &pb.Ack{
 		Status:  true,
@@ -60,28 +104,34 @@ func main() {
 		dlog.ErrorContext(ctx, "Could not get observer", "error", err)
 		os.Exit(1)
 	}
-	defer shutdown(ctx)	
+	defer shutdown(ctx)
 
-	_, span := observer.Tracer.Start(ctx, "DeliveryInit")
-	defer span.End()
+	metrics, err := NewDeliveryMetrics(observer.Meter)
+	if err != nil {
+		dlog.ErrorContext(ctx, "getting metrics", "error", err)
+		os.Exit(1)
+	}
 
 	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		dlog.Error("Could not listen on port 50051", "error", err)
+		dlog.ErrorContext(ctx, "Could not listen on port 50051", "error", err)
 		os.Exit(1)
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterDataServiceServer(grpcServer, &server{})
+	pb.RegisterDataServiceServer(grpcServer, &server{
+		obs: observer,
+		metrics: metrics,
+	})
 	reflection.Register(grpcServer)
 
-	dlog.Info("grpc server started successfully")
+	dlog.InfoContext(ctx, "grpc server started successfully")
 
 	var wg syncutils.MyWaitGroup
 	wg.Go(func() {
-		span.AddEvent("Starting grpc server")
+		dlog.InfoContext(ctx, "starting grpc server")
 		if err := grpcServer.Serve(listener); err != nil {
-			dlog.Error("grpc server error", "error", err)
+			dlog.ErrorContext(ctx, "grpc server error", "error", err)
 		}
 	})
 
@@ -89,9 +139,4 @@ func main() {
 	dlog.WarnContext(ctx, "Received signal", "signal", sig)
 	cancel()
 	wg.Wait()
-	span.AddEvent("recieved signal, gracefully shutdown..")
-	span.SetAttributes(
-		attribute.String("os.signal.name", sig.String()),
-	)
-	span.End()
 }
--- cmd/delivery/telegram.go
+++ cmd/delivery/telegram.go
@@ -9,6 +9,11 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	telemetry "github.com/Nox1KCL/Arbitrage/internal/observer"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/metric"
 )
 
 var telegramAPI = "https://api.telegram.org"
@@ -23,20 +28,44 @@ type telegramResponse struct {
 	Description string `json:"description"`
 }
 
-func sendTelegramMessage(ctx context.Context, chatID int64, text string) error {
+func sendTelegramMessage(ctx context.Context, chatID int64, text string, obs *telemetry.Observe, m *deliveryMetrics) error {
+	start := time.Now()
+	defer func() {
+		duration := time.Since(start).Seconds()
+		m.histogram.Record(ctx, duration, metric.WithAttributes(
+			attribute.String("stage", "sendTelegramMessage"),
+		))
+	}()
+
+	ctx, span := obs.Tracer.Start(ctx, "SendingTelegramMessage")
+	defer span.End()
+
 	token := os.Getenv("TELEGRAM_BOT_API")
 	if token == "" {
+		span.SetStatus(codes.Error, "telegram bot token is missing")
+
 		return fmt.Errorf("TELEGRAM_BOT_API is not set")
 	}
 
 	body, err := json.Marshal(sendMessageRequest{ChatID: chatID, Text: text})
 	if err != nil {
+		span.SetStatus(codes.Error, "could not marshal request")
+		span.RecordError(err)
+		
 		return fmt.Errorf("marshaling request: %w", err)
 	}
 
 	url := fmt.Sprintf("%s/bot%s/sendMessage", telegramAPI, token)
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(body))
 	if err != nil {
+		span.SetStatus(codes.Error, "could not form request")
+		span.RecordError(err)
+
+		m.counter.Add(ctx, 1, metric.WithAttributes(
+			attribute.String("stage", "sendTelegramMessage"),
+			attribute.String("type", "request.create.error"),
+		))
+
 		return fmt.Errorf("creating request: %w", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
@@ -44,21 +73,36 @@ func sendTelegramMessage(ctx context.Context, chatID int64, text string) error {
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
+		span.SetStatus(codes.Error, "could not perform request")
+		span.RecordError(err)
+
+		m.counter.Add(ctx, 1, metric.WithAttributes(
+			attribute.String("stage", "sendTelegramMessage"),
+			attribute.String("type", "request.perform.error"),
+		))
+
 		return fmt.Errorf("sending request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
+		span.SetStatus(codes.Error, "could not read response")
+		span.RecordError(err)
+
 		return fmt.Errorf("reading response: %w", err)
 	}
 
 	var tgResp telegramResponse
 	if err := json.Unmarshal(respBody, &tgResp); err != nil {
+		span.SetStatus(codes.Error, "could not unmarshal telegram response")
+		span.RecordError(err)
+
 		return fmt.Errorf("unmarshaling response: %w", err)
 	}
 
 	if !tgResp.OK {
+		span.AddEvent("telegram !ok result")
 		return fmt.Errorf("telegram error: %s", tgResp.Description)
 	}
 
