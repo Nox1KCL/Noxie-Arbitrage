8d2e31f65714011ee8fa682f42af938ec86d1ab8
Nox1KCL <korshunov.maks09@gmail.com>
2026-08-26 21:57:59 +0300

    fix: improve GetConfig error check cover

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,36 +1,48 @@
 package config
 
 import (
+	"errors"
 	"fmt"
-	"github.com/Nox1KCL/Arbitrage/internal/logger"
 	"os"
 	"path/filepath"
+
+	"github.com/Nox1KCL/Arbitrage/internal/logger"
 
 	"github.com/pelletier/go-toml/v2"
 )
 
 type Config struct {
-	WorkersNum int                 `toml:"workers_num"`
-	AdminID    int                 `toml:"admin_id"`
-	LogsDir    string              `toml:"logs_dir"`
-	LumberConfig     logger.LumberConfig `toml:"logger"`
+	WorkersNum   int                 `toml:"workers_num"`
+	AdminID      int                 `toml:"admin_id"`
+	LogsDir      string              `toml:"logs_dir"`
+	LumberConfig logger.LumberConfig `toml:"logger"`
 }
 
 func GetConfig(path string) (*Config, error) {
 	var doc []byte
 	var err error
+	info, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("path is not exist: %s", path)
+		}
+		return nil, err
+	}
 
-	if path != "" || filepath.IsAbs(filepath.Clean(path)) {
+	if !info.IsDir() && filepath.IsAbs(filepath.Clean(path)) {
 		doc, err = os.ReadFile(path)
 		if err != nil {
 			return nil, fmt.Errorf("reading config file %q: %w", path, err)
 		}
 	} else {
-		return nil, fmt.Errorf("no path provided or not absolute path")
+		return nil, fmt.Errorf("path leads to dir or not absolute path")
 	}
 
 	var cfg Config
 
+	if filepath.Ext(path) != ".toml" {
+		return nil, fmt.Errorf("unexpected file type (accepting only toml, got %s)", filepath.Ext(path))
+	}
 	if err := toml.Unmarshal(doc, &cfg); err != nil {
 		return nil, fmt.Errorf("reading toml doc %q: %w", path, err)
 	}
